7fd58f3d9353739245da2d8e96790214a70e4277
Ronjohnson <phangweijian@gmail.com>
2023-09-06 00:37:35 +0800

    dumb error

--- forms.py
+++ forms.py
@@ -5,5 +5,5 @@ class Login(Form):
     Username = StringField("",[validators.Length(min=1, max=100), validators.DataRequired()])
     Password = PasswordField('', [validators.Length(min=8, max=15, message="Error"), validators.DataRequired()])
 
-class quiz(Form)
-    qn1 = SelectField('please select',choices=[('t1','test1'),('t2','test2'),('t3','test3')],validators=[DataRequired()] )
+class quiz(Form):
+    qn1 = SelectField('please select',choices=[('t1','test1'),('t2','test2'),('t3','test3')], validators=[DataRequired()] )
