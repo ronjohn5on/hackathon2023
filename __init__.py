ec8bb148fe8f1ee3bde643f8037c5c7ce7abd627
Wenjie24 <122337267+Wenjie24@users.noreply.github.com>
2023-09-07 01:27:00 +0800

    Favourite, food details feature

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ def generate_token():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=32))
 
 def create_ingredient():
-     with app.app_context():
+    with app.app_context():
         existing = ingredient.query.all()
         if not existing:
             ingredient1 = ingredient(ingredient_name="Rice")
@@ -68,7 +68,52 @@ def create_food():
                 goal1="Goal 1",
                 goal2="Goal 2",
                 goal3="Goal 3",
-                image = "chicken_rice.webp"
+                image = "chicken_rice.webp",
+                cooking_direction = '''
+                Ingredients:
+    
+                For the Chicken:
+                - 1 whole chicken (about 3-4 pounds)
+                - 1 thumb-sized piece of ginger, peeled and sliced
+                - 2-3 cloves garlic, peeled
+                - Salt, to taste
+                - Water, enough to cover the chicken
+    
+                For the Rice:
+                - 2 cups jasmine rice (or any long-grain rice)
+                - 2 1/2 cups chicken broth (from cooking the chicken)
+                - 1 thumb-sized piece of ginger, peeled and minced
+                - 2-3 cloves garlic, minced
+                - Salt, to taste
+    
+                For the Sauce:
+                - Soy sauce
+                - Sesame oil
+                - Chili sauce (optional)
+    
+                Instructions:
+    
+                1. Rinse the rice under cold water until the water runs clear. Drain and set aside.
+    
+                2. In a large pot, bring enough water to a boil to submerge the whole chicken. Add the ginger, garlic, and salt to the boiling water.
+    
+                3. Carefully add the whole chicken to the boiling water, breast-side down. Cover the pot, reduce the heat to a simmer, and cook for about 40-50 minutes or until the chicken is fully cooked. You can check for doneness by inserting a knife into the thigh; the juices should run clear.
+    
+                4. Once the chicken is cooked, remove it from the pot and place it in a bowl of ice water to cool quickly. This helps to retain the chicken's tenderness and flavor. Once cooled, rub the chicken with a little salt and sesame oil. Set it aside.
+    
+                5. While the chicken is cooling, use the chicken broth you cooked the chicken in to prepare the rice. In a separate pot, heat a little vegetable oil and sauté the minced ginger and garlic until fragrant. Add the rice and stir for a couple of minutes.
+    
+                6. Pour in the chicken broth, and bring it to a boil. Reduce the heat to low, cover the pot, and simmer for about 15-20 minutes or until the rice is cooked and the liquid is absorbed. Fluff the rice with a fork.
+    
+                7. To serve, slice the chicken into bite-sized pieces.
+    
+                8. Prepare the dipping sauce by mixing soy sauce, a bit of sesame oil, and chili sauce (if desired) to taste. You can also add some chopped fresh cilantro and green onions for extra flavor.
+    
+                9. Serve the sliced chicken on a plate with a side of chicken rice and the dipping sauce.
+    
+                Enjoy your homemade chicken rice! This dish is delicious and comforting, with tender chicken and fragrant rice, complemented by the flavorful dipping sauce.
+                '''
+
             )
             food_item2 = food(
                 title="Spaghetti",
@@ -78,11 +123,50 @@ def create_food():
                 goal1="Goal 1",
                 goal2="Goal 2",
                 goal3="Goal 3",
-                image = "spagehti.jpg"
+                image = "spagehti.jpg",
+                cooking_direction='''
+                    Ingredients:
+        
+                    For the Spaghetti:
+                    - 8 ounces (about 1/2 pound) of spaghetti
+                    - Salt, for boiling water
+        
+                    For the Tomato Sauce:
+                    - 1 can (28 ounces) of crushed tomatoes
+                    - 2 cloves garlic, minced
+                    - 1 small onion, finely chopped
+                    - 2 tablespoons olive oil
+                    - 1 teaspoon dried basil
+                    - 1 teaspoon dried oregano
+                    - Salt and pepper, to taste
+                    - Red pepper flakes (optional, for heat)
+                    - Fresh basil leaves, for garnish (optional)
+                    - Grated Parmesan cheese, for topping (optional)
+        
+                    Instructions:
+        
+                    1. Bring a large pot of salted water to a boil. Add the spaghetti and cook according to the package instructions until al dente. Drain and set aside.
+        
+                    2. In a large skillet or saucepan, heat the olive oil over medium heat. Add the minced garlic and chopped onion. Sauté for about 2-3 minutes or until the onion becomes translucent.
+        
+                    3. Add the crushed tomatoes to the skillet, along with dried basil, dried oregano, salt, pepper, and red pepper flakes if you like it spicy. Stir to combine.
+        
+                    4. Reduce the heat to low and let the sauce simmer for about 15-20 minutes, stirring occasionally. This allows the flavors to meld and the sauce to thicken.
+        
+                    5. Taste the sauce and adjust the seasoning as needed.
+        
+                    6. To serve, divide the cooked spaghetti among plates, ladle the tomato sauce over the spaghetti, and garnish with fresh basil leaves and grated Parmesan cheese if desired.
+        
+                    7. Enjoy your homemade spaghetti with tomato sauce!
+        
+                    This classic spaghetti recipe is simple and delicious. You can customize it by adding cooked meatballs, sausages, or vegetables to the sauce if you prefer. Adjust the seasonings to your taste and enjoy!
+                    '''
+
             )
             db.session.add(food_item1)
             db.session.add(food_item2)
             db.session.commit()
+
 
 @app.route('/')
 @app.route('/home')
@@ -130,11 +214,12 @@ def favouriteHandler(food_id):
             # Add the new user to the database
             db.session.add(newFavourite)
             db.session.commit()
-            return 'new favourited'
         else:
-            return 'this item already favourited'
+            db.session.delete(favourited_questionmark)
+            db.session.commit()
 
-    return 'not favourited'
+    return redirect(request.referrer)
+
 
 @app.route('/favourite') #Show favourite
 def favourite():
@@ -147,11 +232,9 @@ def favourite():
     except Exception as e:
         return e
     else:
-        all_food = []
-        for data in favourites:
-            all_food.append(data.food_id_foreign_key)
-        print(all_food)
-    return 'Favourite page'
+        all_favourited = [i.food_id_foreign_key for i in favourites]
+        all_food = [i for i in food.query.all() if i.id in all_favourited]
+        return render_template('favourite.html', favourited=all_food)
 
 
 @app.route('/bmi',methods=['GET', 'POST'])
@@ -219,23 +302,23 @@ def login():
         user = Users.query.filter_by(email=email).first()
 
         if user:
-                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                    # Authentication successful, perform login
-                    # You can use session or Flask-Login for user session management
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                # Authentication successful, perform login
+                # You can use session or Flask-Login for user session management
 
-                    login_user(user)  # Set the user in the session
-                    session['username'] = user.username
-                    session['logged_in'] = True
-                    flash('Login successful!', category='alert-success')
-                    next_page = request.args.get('next') or url_for('home')
+                login_user(user)  # Set the user in the session
+                session['username'] = user.username
+                session['logged_in'] = True
+                flash('Login successful!', category='alert-success')
+                next_page = request.args.get('next') or url_for('home')
 
 
-                    if next_page == url_for('home'):
-                        return redirect(url_for('home'))
-                    # return redirect(next_page)
-                else:
-                    flash('Invalid credentials.', category='alert-danger')
-                    return redirect(url_for('login'))
+                if next_page == url_for('home'):
+                    return redirect(url_for('home'))
+                # return redirect(next_page)
+            else:
+                flash('Invalid credentials.', category='alert-danger')
+                return redirect(url_for('login'))
         else:
             flash('Invalid credentials.', category='alert-danger')
             return redirect(url_for('login'))
@@ -250,22 +333,50 @@ def logout():
     flash('You have been logged out.', category='alert-success')
     return redirect(url_for('home'))
 
+@app.route('/food/<int:id>')
+def food_details(id):
+    try:
+        food_item = food.query.filter_by(id=id).first()
+    except Exception as e:
+        return 'eroror is', e
+    else:
+        print("Food no error: ", food_item)
+
+        favourite_list = None
+        # Get user favourite list
+        if session.get('logged_in'):
+            account_id = Users.query.filter_by(username=session.get('username')).first().id
+            favourite_list = [i.food_id_foreign_key for i in
+                              Favourites.query.filter_by(account_id_foreign_key=account_id).all()]
+            print(favourite_list)
+
+        return render_template('food_details.html', food_item=food_item, favourite_list=favourite_list)
+
+
 @app.route('/food_items')
 def display_food_items():
     # Query the food table to retrieve all food items
     food_items = food.query.all()
     return render_template('food_items.html', food_items=food_items)
 
+
+
 @app.route('/food_items2')
 def display_food_items2():
     # Query the food table to retrieve all food items
     food_items = food.query.all()
-    return render_template('food_items2.html', food_items=food_items)
+    favourite_list = None
+    #Get user favourite list
+    if session.get('logged_in'):
+        account_id = Users.query.filter_by(username=session.get('username')).first().id
+        favourite_list = [i.food_id_foreign_key for i in Favourites.query.filter_by(account_id_foreign_key=account_id).all()]
+        print(favourite_list)
+    return render_template('food_items2.html', food_items=food_items, favourite_list=favourite_list)
 
 @app.route('/quiz',methods=['GET', 'POST'])
 def quizPage():
     quizForm = quiz(request.form)
-    
+
     return render_template('quiz.html',form=quizForm)
 
 @app.route('/profile')
